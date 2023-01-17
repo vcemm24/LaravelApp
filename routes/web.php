f6494ddd09236b042d87388b85732b7e4d1bec80
vcemm24 <vcemm24@gmail.com>
2023-01-17 11:16:50 +0100

    fist config

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings');
+   // return response('<h1>Hello World</h1>',404)
+    //return response (header('foo','bar');
+    //->header('Content-Type','text/plain');
+    //->header('foo','bar');
 });
+/* Route::get('/posts/{id}', function ($id) {
+    //return view('welcome');
+    //dd($id);
+    return response('Post '.$id);
+})->where('id','[0-9]+'); */
+Route::get('/search', function (Request $request) {
+    //return view('welcome');
+    //dd($id);
+    //dd($request);
+    return $request->name.' '.$request->city;
+
+});
+
+
